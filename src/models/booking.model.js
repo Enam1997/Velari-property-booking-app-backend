576859426f50a2fdf1a7929efb00146e9631931d
Enam <mdenamahmedchowdhury@gmail.com>
2024-08-16 18:09:49 +0600

    Bookinkg Schema Add

---
+++ src/models/booking.model.js
@@ -0,0 +1,62 @@
+import mongoose, { Schema } from "mongoose";
+
+const bookingRoomItemSchema = new Schema({
+  roomType: {
+    type: String,  // Reference the roomTypeName from Property schema
+    required: true,
+  },
+  roomTypeId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Property.roomTypes',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  pricePerRoom: {
+    type: Number,
+    required: true,
+  },
+  totalRoomPrice: {
+    type: Number,
+    required: true,
+  }
+});
+
+const bookingSchema = new Schema(
+  {
+    property: {
+      type: Schema.Types.ObjectId,
+      ref: "Property",
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    checkInDate: {
+      type: Date,
+      required: true,
+    },
+    checkOutDate: {
+      type: Date,
+      required: true,
+    },
+    bookedRooms: [bookingRoomItemSchema], // Array of booked rooms
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Booked", "Cancelled", "Completed"],
+      default: "Booked",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Booking = mongoose.model("Booking", bookingSchema);
